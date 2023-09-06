568e1381ab5afe63a93798ad0fb86ea8f7d5b37e
Kresswell <130984721+Kresswell@users.noreply.github.com>
2023-09-06 15:03:53 +0300

    config.py

--- sample_config.py
+++ sample_config.py
@@ -11,7 +11,7 @@ if HEROKU:
     API_HASH = environ["API_HASH"]
     SESSION_STRING = environ[
         "SESSION_STRING"
-    ] # Check Readme for session
+    ]  # Check Readme for session
     ARQ_API_KEY = environ["ARQ_API_KEY"]
     CHAT_ID = int(environ["CHAT_ID"])
     DEFAULT_SERVICE = environ.get("DEFAULT_SERVICE") or "youtube"
